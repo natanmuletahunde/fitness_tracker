4c6380f1d1b02c35f4131d8d8744ce941b9b0ede
pivnati <natanmuleta911@gmail.com>
2024-08-25 18:22:35 -0700

    modify the app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,26 @@
-import './App.css'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
+import Home from './pages/Home';
+import About from './pages/About';
+import Services from './pages/Services';
+import Contact from './pages/Contact';
 
 function App() {
-
-
   return (
-     <div>
-      <h1 className='bg-green-500'> this piv nati</h1>
-       </div>
-  )
+    <Router>
+      <Navbar />
+      <main>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/services" element={<Services />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+      </main>
+      <Footer />
+    </Router>
+  );
 }
 
-export default App
+export default App;
