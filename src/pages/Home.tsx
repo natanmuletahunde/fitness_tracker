e33094d17d907943b636c63f54d96d90d2bfb8a0
pivnati <natanmuleta911@gmail.com>
2024-08-25 18:20:10 -0700

    c create the home page

---
+++ src/pages/Home.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import HeroSection from '../components/HeroSection';
+import Services from '../components/Services';
+
+const Home = () => {
+  return (
+    <>
+      <HeroSection />
+      <Services />
+    </>
+  );
+};
+
+export default Home;
