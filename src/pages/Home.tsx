014c7afd91ed80ee4778867e0b5117a508d7b901
pivnati <natanmuleta911@gmail.com>
2024-08-25 19:21:48 -0700

    make the imges to take the url of the img

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -6,21 +6,21 @@ interface Blog {
   id: number;
   title: string;
   summary: string;
-  image: string;  // Added image property
+  image: string;  // URL of the image
 }
 
 const Home: React.FC = () => {
-  // Define an array of blog data with images
+  // Define an array of blog data with image URLs
   const blogs: Blog[] = [
-    { id: 1, title: 'Blog Post 1', summary: 'Summary of blog post 1', image: '/images/blog1.jpg' },
-    { id: 2, title: 'Blog Post 2', summary: 'Summary of blog post 2', image: '/images/blog2.jpg' },
-    { id: 3, title: 'Blog Post 3', summary: 'Summary of blog post 3', image: '/images/blog3.jpg' },
-    { id: 4, title: 'Blog Post 4', summary: 'Summary of blog post 4', image: '/images/blog4.jpg' },
-    { id: 5, title: 'Blog Post 5', summary: 'Summary of blog post 5', image: '/images/blog5.jpg' },
-    { id: 6, title: 'Blog Post 6', summary: 'Summary of blog post 6', image: '/images/blog6.jpg' },
-    { id: 7, title: 'Blog Post 7', summary: 'Summary of blog post 7', image: '/images/blog7.jpg' },
-    { id: 8, title: 'Blog Post 8', summary: 'Summary of blog post 8', image: '/images/blog8.jpg' },
-    { id: 9, title: 'Blog Post 9', summary: 'Summary of blog post 9', image: '/images/blog9.jpg' },
+    { id: 1, title: 'Blog Post 1', summary: 'Summary of blog post 1', image: 'https://example.com/images/blog1.jpg' },
+    { id: 2, title: 'Blog Post 2', summary: 'Summary of blog post 2', image: 'https://example.com/images/blog2.jpg' },
+    { id: 3, title: 'Blog Post 3', summary: 'Summary of blog post 3', image: 'https://example.com/images/blog3.jpg' },
+    { id: 4, title: 'Blog Post 4', summary: 'Summary of blog post 4', image: 'https://example.com/images/blog4.jpg' },
+    { id: 5, title: 'Blog Post 5', summary: 'Summary of blog post 5', image: 'https://example.com/images/blog5.jpg' },
+    { id: 6, title: 'Blog Post 6', summary: 'Summary of blog post 6', image: 'https://example.com/images/blog6.jpg' },
+    { id: 7, title: 'Blog Post 7', summary: 'Summary of blog post 7', image: 'https://example.com/images/blog7.jpg' },
+    { id: 8, title: 'Blog Post 8', summary: 'Summary of blog post 8', image: 'https://example.com/images/blog8.jpg' },
+    { id: 9, title: 'Blog Post 9', summary: 'Summary of blog post 9', image: 'https://example.com/images/blog9.jpg' },
   ];
 
   return (
