2c67c92a8fd68fe9e7558b8154ae38c4026a11af
pivnati <natanmuleta911@gmail.com>
2024-08-25 19:38:40 -0700

    clear the blog and summary  in all posts

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -12,15 +12,12 @@ interface Blog {
 const Home: React.FC = () => {
   // Define an array of blog data with image URLs
   const blogs: Blog[] = [
-    { id: 1, title: 'Blog Post 1', summary: 'Summary of blog post 1', image: 'https://example.com/images/blog1.jpg' },
-    { id: 2, title: 'Blog Post 2', summary: 'Summary of blog post 2', image: 'https://example.com/images/blog2.jpg' },
-    { id: 3, title: 'Blog Post 3', summary: 'Summary of blog post 3', image: 'https://example.com/images/blog3.jpg' },
-    { id: 4, title: 'Blog Post 4', summary: 'Summary of blog post 4', image: 'https://example.com/images/blog4.jpg' },
-    { id: 5, title: 'Blog Post 5', summary: 'Summary of blog post 5', image: 'https://example.com/images/blog5.jpg' },
-    { id: 6, title: 'Blog Post 6', summary: 'Summary of blog post 6', image: 'https://example.com/images/blog6.jpg' },
-    { id: 7, title: 'Blog Post 7', summary: 'Summary of blog post 7', image: 'https://example.com/images/blog7.jpg' },
-    { id: 8, title: 'Blog Post 8', summary: 'Summary of blog post 8', image: 'https://example.com/images/blog8.jpg' },
-    { id: 9, title: 'Blog Post 9', summary: 'Summary of blog post 9', image: 'https://example.com/images/blog9.jpg' },
+    { id: 1, title: 'Blog Post 1', summary: 'Summary of blog post 1', image: 'https://thumbs.dreamstime.com/b/power-muscular-bodybuilder-guy-doing-pullups-gym-fitness-man-black-white-image-pumping-up-lats-muscles-91597926.jpg' },
+    { id: 2, title: 'Blog Post 2', summary: 'Summary of blog post 2', image: 'https://manofmany.com/wp-content/uploads/2021/01/Fitness-Trends-2021-c.jpg' },
+    { id: 3, title: 'Blog Post 3', summary: 'Summary of blog post 3', image: 'https://www.jamesallenphoto.com/uploads/7/8/3/1/78318796/fitness-photography-guys-05-2000-px_orig.jpg' },
+    { id: 4, title: 'Blog Post 4', summary: 'Summary of blog post 4', image: 'https://i.ytimg.com/vi/Mg71qKLF9Fk/maxresdefault.jpg' },
+    { id: 5, title: 'Blog Post 5', summary: 'Summary of blog post 5', image: 'https://i.ytimg.com/vi/puzd9MrO6yI/hq720.jpg?sqp=-oaymwEhCK4FEIIDSFryq4qpAxMIARUAAAAAGAElAADIQj0AgKJD&rs=AOn4CLDDEtW7_ClozkW2Ng54Uw5PkdiXdQ.jpg' },
+    { id: 6, title: 'Blog Post 6', summary: 'Summary of blog post 6', image: 'https://i.ytimg.com/vi/D30ye257-Ng/maxresdefault.jpg' },
   ];
 
   return (
@@ -31,7 +28,9 @@ const Home: React.FC = () => {
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6">
         {blogs.map(blog => (
           <div key={blog.id} className="bg-white shadow-md rounded-lg overflow-hidden">
-            <img src={blog.image} alt={blog.title} className="w-full h-48 object-cover" />
+            <div className="relative w-full h-48">
+              <img src={blog.image} alt={blog.title} className="absolute inset-0 w-full h-full object-contain" />
+            </div>
             <div className="p-6">
               <h2 className="text-xl font-semibold mb-2">{blog.title}</h2>
               <p className="text-gray-600">{blog.summary}</p>
