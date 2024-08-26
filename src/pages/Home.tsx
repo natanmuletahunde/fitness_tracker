381b57481ca0beab694e9711a2d3d2da7d7a0625
pivnati <natanmuleta911@gmail.com>
2024-08-25 19:14:30 -0700

    clear the comment

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -25,8 +25,8 @@ const Home: React.FC = () => {
 
   return (
     <div className="container mx-auto px-4 py-8">
-      <HeroSection /> {/* Include the HeroSection component */}
-      <Services /> {/* Include the Services component */}
+      <HeroSection />
+      <Services /> 
       <h1 className="text-3xl font-bold mb-6 mt-12">Latest Blog Posts</h1>
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6">
         {blogs.map(blog => (
