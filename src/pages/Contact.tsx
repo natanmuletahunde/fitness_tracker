0a3723eaf3a91492d4970e316a564df447adf7fd
pivnati <natanmuleta911@gmail.com>
2024-08-25 18:21:16 -0700

    c create the contact page

---
+++ src/pages/Contact.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import ContactForm from '../components/ContactForm';
+
+const Contact = () => {
+  return (
+    <section>
+      <ContactForm />
+    </section>
+  );
+};
+
+export default Contact;
